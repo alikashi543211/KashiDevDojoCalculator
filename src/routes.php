624db8c9154768e18b9d2275c8e818bd32c83fae
mac <hafiz@bitsoftsolution.com>
2023-10-23 16:11:02 +0500

    namespace change

--- src/routes.php
+++ src/routes.php
@@ -6,5 +6,5 @@ Route::get('k-calculator', function(){
 	echo 'Hello from the calculator package!';
 });
 
-Route::get('k-add/{a}/{b}', 'Devdojo\Calculator\CalculatorController@add');
-Route::get('k-subtract/{a}/{b}', 'Devdojo\Calculator\CalculatorController@subtract');
+Route::get('k-add/{a}/{b}', 'KashiDevdojo\Calculator\CalculatorController@add');
+Route::get('k-subtract/{a}/{b}', 'KashiDevdojo\Calculator\CalculatorController@subtract');
